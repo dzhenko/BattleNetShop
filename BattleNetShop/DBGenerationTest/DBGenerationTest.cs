db65bd385583a683deacdb39982fd6a5bce5e995
fast4y <dzhenko@gmail.com>
2014-08-29 17:02:03 +0300

    Added empty classes for the seeds for MongoDB and The zippedFolders holding excel files (2003 .xls)

---
+++ BattleNetShop/DBGenerationTest/DBGenerationTest.cs
@@ -0,0 +1,22 @@
+﻿namespace DBGenerationTest
+{
+    using System;
+    using System.Linq;
+
+    using BattleNetShop.Data;
+    using BattleNetShop.Model;
+
+    public class Tests
+    {
+        public static void Main()
+        {
+            var db = new BattleNetShopDbContext();
+
+            Console.WriteLine(db.Products.FirstOrDefault());
+
+            BattleNetShop.Initial.MongoDBSeed.MongoDBSeed.SeedData();
+
+            BattleNetShop.Initial.ZippedExcelFilesSeed.ZippedExcelFilesSeed.SeedData();
+        }
+    }
+}
