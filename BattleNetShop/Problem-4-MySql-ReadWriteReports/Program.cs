bbd7aed006ada81261c192eafac20166ef5a588a
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-08-30 16:53:47 +0300

    nzhul commit

---
+++ BattleNetShop/Problem-4-MySql-ReadWriteReports/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Problem_4_MySql_ReadWriteReports
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using BattleNetShop.Data.MySqlTelerikDA;
+    class Program
+    {
+        static void Main()
+        {
+            MySqlTelerikHandler mysqlReporter = new MySqlTelerikHandler();
+            var allReports = mysqlReporter.ReadAllReports();
+
+            // Example for using the reports
+            foreach (var report in allReports)
+            {
+                Console.WriteLine("ID: {0}", report.ProductID);
+                Console.WriteLine("ProductName: {0}", report.ProductName);
+                Console.WriteLine("VendorName: {0}", report.VendorName);
+                Console.WriteLine("Total Quantity Sold: {0}", report.TotalQuantitySold);
+                Console.WriteLine("Total Incomes: {0}", report.TotalIncomes);
+
+            }
+            Console.WriteLine("------------------------");
+        }
+    }
+}
