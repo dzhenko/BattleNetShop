da5e5fa6405e5ef59c50d813afd0552d3433b38c
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-08-30 16:01:00 +0300

    JSON Reports - Change Property Names

--- BattleNetShop/BattleNetShop.Data.JSON/JSONReport.cs
+++ BattleNetShop/BattleNetShop.Data.JSON/JSONReport.cs
@@ -8,10 +8,19 @@
     using Newtonsoft.Json;
     public class JSONReport
     {
+        [JsonProperty(PropertyName = "product-id")]
         public int ProductID { get; set; }
+
+        [JsonProperty(PropertyName = "product-name")]
         public string ProductName { get; set; }
+
+        [JsonProperty(PropertyName = "vendor-name")]
         public string VendorName { get; set; }
+
+        [JsonProperty(PropertyName = "total-quantity-sold")]
         public int TotalQuantitySold { get; set; }
+
+        [JsonProperty(PropertyName = "total-incomes")]
         public int TotalIncomes { get; set; }
 
         public JSONReport(int productID, string productName, string vendorName, int totalQuantitySold, int totalIncomes)
