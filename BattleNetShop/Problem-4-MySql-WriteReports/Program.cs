e7bdf4d21a2c0341a8a935c756f4e93933e5cff8
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-08-30 17:41:50 +0300

    Write report to mysql - not ready yet - need testing

---
+++ BattleNetShop/Problem-4-MySql-WriteReports/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Problem_4_MySql_WriteReports
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using BattleNetShop.Data.MySqlTelerikDA;
+    class Program
+    {
+        static void Main()
+        {
+            MySqlTelerikHandler reportWriter = new MySqlTelerikHandler();
+            reportWriter.WriteReport(2, "AwesomeItem", "Blizzard", 111, 222);
+        }
+    }
+}
