6ada50cce0522379af4ed2af969fe3d68de1ab4b
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-08-31 15:34:39 +0300

    PDF Reported Working in separate solution
    I have used PDFSharp + MigraDoc Library for the pdf creation. You can
    get them via nuget.
    I'v set the solution to auto load them but if you have any problem -
    please write to me.

---
+++ PDFReporter/BattleNetShop.Data.PDF/ReportRow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleNetShop.Data.PDF
+{
+    public class ReportRow
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+        public string Vendor { get; set; }
+        public double Sum { get; set; }
+
+        public ReportRow(string productName, int quantity, double unitPrice, string vendor)
+        {
+            this.ProductName = productName;
+            this.Quantity = quantity;
+            this.UnitPrice = unitPrice;
+            this.Vendor = vendor;
+            this.Sum = this.Quantity * this.UnitPrice;
+        }
+    }
+}
---
+++ PDFReporter/Problem-2-PDFGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem_2_PDF_Reports
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using BattleNetShop.Data.PDF;
+    class Program
+    {
+        static void Main()
+        {
+            PDFHandler PDFMaker = new PDFHandler();
+
+
+            List<ReportRow> allReports = new List<ReportRow>();
+
+            ReportRow simpleReport1 = new ReportRow("Sword of Justice", 455, 2.5, "Blizzard");
+            ReportRow simpleReport2 = new ReportRow("Gold", 20000, 0.9, "Random Vendor");
+            ReportRow simpleReport3 = new ReportRow("Stone of Jordan", 1455, 4.5, "Blizzard");
+            ReportRow simpleReport4 = new ReportRow("Awesome Amulet", 2455, 1.5, "Blizzard");
+            allReports.Add(simpleReport1);
+            allReports.Add(simpleReport2);
+            allReports.Add(simpleReport3);
+            allReports.Add(simpleReport4);
+
+
+            PDFMaker.TestPdfCreate(allReports);
+        }
+    }
+}
