4659d22f8aa1b2c19a3f55e9ea43d7beb822375b
damy90 <dany90@gmail.com>
2014-09-01 20:17:51 +0300

    Implemented GenerateAllLocationsReport

--- BattleNetShop/BattleNetShop.Data.XML/XmlData.cs
+++ BattleNetShop/BattleNetShop.Data.XML/XmlData.cs
@@ -4,12 +4,16 @@
 
     using BattleNetShop.Model;
     using BattleNetShop.ReportsModel;
+    using System.Xml;
+    using System.Globalization;
+    using BattleNetShop.Logic;
 
     public class XmlData : IXmlData
     {
         public IEnumerable<VendorExpense> GetAllVendorExpenses()
         {
             var allVendorExpenses = new List<VendorExpense>();
+            IBattleNetShopSqlServerData msSqlData;
 
             // foreach vendor name
                 // foreach expense
@@ -43,26 +47,46 @@
 
         public void GenerateAllLocationsReport(IEnumerable<LocationReport> locationReports)
         {
-            throw new System.NotImplementedException();
+            // TODO moove some functionality to XmlHandler
+            XmlDocument report = new XmlDocument();
+            XmlDeclaration xmlDeclaration = report.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = report.CreateElement("sales");
+            report.AppendChild(root);
+            report.InsertBefore(xmlDeclaration, root);
+
+            foreach (LocationReport locationReport in locationReports)
+            {
+                XmlElement sale = report.CreateElement("location");
+                sale.SetAttribute("name", locationReport.LocationName);
+                root.AppendChild(sale);
+                foreach (LocationReportEntry entry in locationReport.Entries)
+                {
+                    XmlElement summary = report.CreateElement("summary");
+                    summary.SetAttribute("date", entry.Date.ToString("dd-MMM-yyyy", CultureInfo.InvariantCulture));
+                    summary.SetAttribute("total-sum", entry.TotalSum.ToString());
+                    sale.AppendChild(summary);
+                }
+            }
+            report.Save("../../../../Reports/XmlReports/XML-all-locations-report.xml");
         }
         /*
 <?xml version="1.0" encoding="utf-8">
 <sales>
-  <sale location="location name here">
+  <location name="location name here">
     <summary date="20-Jul-2013" total-sum="54.75" />
     <summary date="21-Jul-2013" total-sum="40.35" />
     <summary date="22-Jul-2013" total-sum="40.60" />
-  </sale>
-  <sale vendor="Targovishte Bottling Company Ltd.">
+  </location>
+  <location name="Targovishte Bottling Company Ltd.">
     <summary date="20-Jul-2013" total-sum="150.20" />
     <summary date="21-Jul-2013" total-sum="709.30" />
     <summary date="22-Jul-2013" total-sum="249.40" />
-  </sale>
-  <sale vendor="Zagorka Corp.">
+  </location>
+  <location name="Zagorka Corp.">
     <summary date="20-Jul-2013" total-sum="144.80" />
     <summary date="21-Jul-2013" total-sum="341.59" />
     <summary date="22-Jul-2013" total-sum="385.80" />
-  </sale>
+  </location>
          */
     }
 }
