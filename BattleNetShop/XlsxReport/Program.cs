58691f39341844e6f296609f2f3b9295e289ce2a
yyankova <yana.o.yankova@gmail.com>
2014-09-01 16:13:47 +0300

    Generate Report based on the two tables form MySQL and SQLite

--- BattleNetShop/XlsxReport/Program.cs
+++ BattleNetShop/XlsxReport/Program.cs
@@ -1,23 +1,40 @@
-﻿using BattleNetShop.Data.Excel.Xlsx;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace XlsxReport
+﻿namespace GenerateXlsxReportTest
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using BattleNetShop.Data.Excel.Xlsx;
+    using BattleNetShop.Data.SqLite;
+    using BattleNetShop.Data.MySql;
+    
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //Get SQLite data
+            SqLiteDataSeeder seeder = new SqLiteDataSeeder();
+            seeder.CreateSQLiteTableProductsTaxes();
+            seeder.SeedTableProductsTaxes();
+            SqLiteData sqliteData = new SqLiteData();
+            var productsTaxes = sqliteData.ReadProductTaxes();
+            //Get MySQL data
+            var salesReport = new List<Salereport>();
+            for (int i = 0; i < productsTaxes.Count; i++)
+            {
+                salesReport.Add(new Salereport()
+                {
+                    Product_id = i,
+                    ProductName = productsTaxes.ElementAt(i).ProductName,
+                    VendorName = "Vendor#" + i.ToString(),
+                    TotalQuantitySold = i + 10,
+                    TotalIncomes = i * 10
+                });
+            }
+
             var xlsxHandler = new ExcelXlsxHandler();
-            var reportData = new List<Tuple<string, decimal, decimal, decimal>>();
-            reportData.Add(new Tuple<string, decimal, decimal, decimal>("Item1", 1m, 2m, 3m));
-            reportData.Add(new Tuple<string, decimal, decimal, decimal>("Item2", 1m, 2m, 3m));
-            reportData.Add(new Tuple<string, decimal, decimal, decimal>("Item3", 1m, 2m, 3m));
-            reportData.Add(new Tuple<string, decimal, decimal, decimal>("Item4", 1m, 2m, 5m));
-            xlsxHandler.GenerateVendorsFinancialResultsReport(reportData, "test.xlsx");
+            var reportData = xlsxHandler.GenerateVendorsFinancialResultReport(productsTaxes, salesReport);
+            xlsxHandler.GenerateVendorsFinancialResultFile(reportData, "test.xlsx");
         }
     }
 }
